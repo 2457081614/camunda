e5ee8572b2b104e6ece96ec82c34504ae0b58d6b
git <2457081614@qq.com>
2020-08-20 18:15:34 +0800

    111

--- src/test/java/com/example/camunda/DeployTest.java
+++ src/test/java/com/example/camunda/DeployTest.java
@@ -1,6 +1,8 @@
 package com.example.camunda;
 
 import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.repository.Deployment;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,10 +18,22 @@ public class DeployTest {
     @Autowired
     private RepositoryService repositoryService;
 
+
+    @Autowired
+    private RuntimeService runtimeService;
+
     @Test
     public void test()
     {
-        repositoryService.createDeployment().addClasspathResource("processes/diagram_1.bpmn").name("test").
+        Deployment deploy = repositoryService.createDeployment().addClasspathResource("processes/diagram_1.bpmn").name("test").
                 deploy();
+        System.out.println(deploy.getId());
+    }
+
+
+    @Test
+    public void test1()
+    {
+        runtimeService.startProcessInstanceByKey("Process_1hi34yh");
     }
 }
